99a57ee310dc4adddf2a063b9296c33d47472bcb
Uriel Bravo <uriel.bravo.lara@gmail.com>
2020-05-22 03:15:17 -0700

    changes to files and gitignore

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,24 @@
 import App from 'next/app';
 import Head from 'next/head';
 import { AppProvider } from '@shopify/polaris';
-import '@shopify/polaris/styles.css';
 import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
+import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
-import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include',
+  },
+});
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
     return (
       <React.Fragment>
         <Head>
@@ -18,9 +26,11 @@ class MyApp extends App {
           <meta charSet="utf-8" />
         </Head>
         <Provider config={config}>
-            <AppProvider i18n={translations}>
-                <Component {...pageProps} />
-            </AppProvider>
+          <AppProvider i18n={translations}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
         </Provider>
       </React.Fragment>
     );
